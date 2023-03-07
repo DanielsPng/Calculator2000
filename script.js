1e62e47a914ad4dcb56c56908e094fe6a892382d
DanielsPng <danielsouza_ioio@outlook.com>
2023-03-07 22:48:49 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ script.js
@@ -0,0 +1,33 @@
+
+var result = document.getElementById('result');
+
+function insert(num) {
+   var number = document.getElementById('result').innerHTML;
+
+   if(number.length == 16){
+    return;
+   }
+   else{
+    document.getElementById('result').innerHTML = number + num;
+   }
+  
+}
+
+function clean(){
+    document.getElementById('result').innerHTML = "";
+}
+
+function back(){
+    var result = document.getElementById('result').innerHTML;
+    document.getElementById('result').innerHTML = result.substring(0, result.length -1);
+}
+
+function calc(){
+    var result = document.getElementById('result').innerHTML;
+    if(result){
+        document.getElementById('result').innerHTML = eval(result);
+    }
+    else{
+        document.getElementById('result').innerHTML = "Insíra algo"
+    }
+}
